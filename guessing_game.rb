3f189577b17422371cbe31fe7d17d9cddcc37608
Angela Coates <angela@angelamarii.com>
2017-03-21 17:17:33 -0400

    step2

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,44 +1,22 @@
 
-number = 58
-# number = rand(100)
+number = rand(1..100)
+counter=0
 
 loop do
   puts "Guess my favorite number!"
   guess = gets.chomp.to_i
+  counter += 1
+  if counter == 5
+    puts "Too many attempts! You lose."
+    break
+  end
   if guess < number
     puts "Sorry, that number is too low. Please try
-   again "
+   again."
   elsif guess > number
-    puts "Sorry, that number is too high. Please try again"
+    puts "Sorry, that number is too high. Please try again."
   elsif guess == number
     puts "That is the correct number! Yay! You win!"
     break
   end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# #if the users guess is less than 58 then
-# #put that the user guessed too low, have them
-# #guess again
-# guess=gets.chomp
-# #if the users guess is higher than 58 then
-# #put that the user guessed too high, have them
-# #guess again
-# guess=gets.chomp
-# #if the users guess is the right answer
-# #put that the user guessed correctly.
-# #if the user guessed more than 5 times, tell the user
-# #that they've lost, break.
